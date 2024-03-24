5a44a2e672497944a26bbf36d3c01852e648e654
Siddharth Rajesh Jiyani <120766410+SiddharthJiyani@users.noreply.github.com>
2024-03-25 00:29:24 +0530

    Update index.js

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -3,7 +3,6 @@ const app = express() ;
 const db = require("./config/database") ;
 var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
-const passport = require('passport');
 const User = require("./models/User") ;
 const authRoutes = require("./routes/auth");
 const songRoutes = require("./routes/song");
@@ -25,41 +24,6 @@ app.listen(port , ()=>{
 
 db.connectDB() ;
 
-// //setup passport-jwt
-// app.use(passport.initialize());
-// var opts = {}
-// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = process.env.JWT_SECRET;
-
-// passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
-//     console.log("jwt_payload" ,  jwt_payload);
-//     await User.findOne({id: jwt_payload.sub}, 
-//     // User.findOne({id: jwt_payload.identifier},
-//         // function(err, user) {
-//         //     if (err) {
-//         //         return done(err, false);
-//         //     }
-//         //     if (user) {
-//         //         return done(null, user);
-//         //     } else {
-//         //         return done(null, false);
-//         //         // or you could create a new account
-//         //     }
-//         // }
-
-//         )
-//         .then(user => {
-//             if (user) {
-//                 return done(null, user);
-//             } else {
-//                 return done(null, false);
-//                 // or you could create a new account
-//             }
-//         })
-//         .catch(err => {
-//             return done(err, false);
-//         });
-// }));
 
 const cookieParser = require("cookie-parser") ;
 app.use(cookieParser()) ;
